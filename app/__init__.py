8454b54aeec0e85ec826e28464bc3bb97e96b8fd
cyq7on <cyq7on@qq.com>
2018-08-23 11:09:24 +0800

    Use web form

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 # coding:utf8
 
 from flask import Flask
+from configy import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
+# print(app.config['SECRET_KEY'])
 from app import routes
